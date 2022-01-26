33d31334692279eab7b1ab5b4b120c2ca30bc7e8
Manthan Khandale <manthan@kisan.net>
2022-01-26 22:39:10 +0530

    example file updated

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:pretty_json/pretty_json.dart';
 
@@ -5,51 +7,52 @@ void main() {
   runApp(const MyApp());
 }
 
-const String jsonData = '''
-{
-  'id':1,
-  'name':'Pretty Json',
-  'isFlutterAwesome':true,
-}
-''';
-
-const responseBody = '''
-[
-  {
-    "id": 1,
-    "name": "Manthan Khandale",
-    "username": "mantyk",
-    "email": "manthan@flutter.com",
-    "hobbies": [
+final dartMap = {
+  "id": 24,
+  "name": "Manthan Khandale",
+  "score": 7.6,
+  "socials": null,
+  "hobbies": [
     "Music",
-    Filmmaking     
-    ],
-    "isFlutterCool": true,
-    "company": {
-      "name": "Romaguera-Crona",
-      "catchPhrase": "Multi-layered client-server neural-net",
-      "bs": "harness real-time e-markets"
-    }
-  }
-  
-]
- 
-
-''';
+    "Filmmaking",
+  ],
+  "isFlutterCool": true,
+};
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Pretty Json Example'),
-        ),
-        body: SingleChildScrollView(
-          child: PrettyJson(
-            data: responseBody,
+          title: const Text(
+            'Pretty Json Example',
           ),
+        ),
+        body: Column(
+          children: [
+            PrettyJson(
+              data: jsonEncode(dartMap),
+              indentLength: 5,
+              keyColor: Colors.green,
+              backgroundColor: Colors.black,
+              boolColor: Colors.white,
+              nullColor: Colors.redAccent,
+              stringColor: Colors.cyan,
+              curlyBracketColor: Colors.yellow,
+              doubleColor: Colors.deepOrange,
+              squareBracketColor: Colors.amber,
+              commaColor: Colors.yellow,
+              colonColor: Colors.purple,
+              intColor: Colors.lime,
+              textStyle: const TextStyle(
+                fontSize: 20,
+                letterSpacing: 2,
+              ),
+            ),
+          ],
         ),
       ),
     );
